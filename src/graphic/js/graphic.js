fac5c8b06f9df6744a5475374f2a858db19588e5
erxclau <ericlau41802@gmail.com>
2023-02-10 17:02:10 -0500

    Add data sheet

--- src/graphic/js/graphic.js
+++ src/graphic/js/graphic.js
@@ -12,6 +12,12 @@ import setDisplayOptions from "./util/set-display";
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const draw = async () => {
+  const filtered = data.map((d) => ({
+    year: +d.Year,
+    zipcode: d.ZIP_Code,
+    count: +d.Count,
+  }));
+
   const zipcodes = new Set(
     template
       .filter((d) => d.localityType === "ZIPCODE")
@@ -22,9 +28,7 @@ const draw = async () => {
     (d) => d.localityType === "NEIGHBORHOOD"
   );
 
-  const zdata = data
-    .map((d) => ({ year: +d.Year, zipcode: d.ZIP_Code, count: +d.Count }))
-    .filter((d) => zipcodes.has(d.zipcode));
+  const zdata = filtered.filter((d) => zipcodes.has(d.zipcode));
 
   const zmap = d3.rollup(
     zdata,
@@ -67,8 +71,39 @@ const draw = async () => {
 
   const nmap = d3.rollup(
     ndata,
-    (v) => new Map(v.map((d) => [new Date(d.year, 0, 0), d.count])),
+    (v) => new Map(v.map((d) => [new Date(d.year, 1, 1), d.count])),
     (d) => d.id
+  );
+
+  console.log(
+    d3.csvFormat(
+      template.map((d) => {
+        const v =
+          d.localityType === "ZIPCODE"
+            ? zmap.get(d.localityName)
+            : nmap.get(d.localityId);
+        const keys = Array.from(v.keys());
+        const values = Array.from(v.values());
+
+        return {
+          ...d,
+          image:
+            d.localityType === "ZIPCODE"
+              ? `${d.localityName}.png`
+              : `${d.localityId}.png`,
+          localityHighNumber: d3.max(values),
+          localityHighYear: keys[d3.maxIndex(values)].getFullYear(),
+          localityChange:
+            values.at(-1) - values.at(0) > 0 ? "increased" : "decreased",
+          localityChangePercent:
+            values.at(0) === 0
+              ? "0%"
+              : d3.format(".0%")(
+                  Math.abs(values.at(-1) - values.at(0)) / values.at(0)
+                ),
+        };
+      })
+    )
   );
 
   const figure = d3.select("figure");
@@ -92,32 +127,42 @@ const draw = async () => {
 
   const nnames = d3.index(neighborhoods, (d) => d.localityId);
 
-  let redraw = false;
-  for await (const neighborhood of nindex.keys()) {
-    update(neighborhood, nnames.get(neighborhood).localityName, redraw);
-    await sleep(1500);
-    await downloadImage(
-      JSON.stringify({
-        entry: neighborhood,
-        format: "png",
-        width,
-      })
-    );
-    redraw = true;
-  }
+  const norcal = d3.rollup(
+    zdata,
+    (v) => d3.sum(v, (d) => d.count),
+    (d) => new Date(d.year, 1, 1)
+  );
 
-  for await (const zipcode of zipcodes) {
-    update(zipcode, null, redraw);
-    await sleep(1500);
-    await downloadImage(
-      JSON.stringify({
-        entry: zipcode,
-        format: "png",
-        width,
-      })
-    );
-    redraw = true;
-  }
+  svg
+    .datum(norcal)
+    .call(build(new LineChart().height(400).margin({ left: 55 })));
+
+  // let redraw = false;
+  // for await (const neighborhood of nindex.keys()) {
+  //   update(neighborhood, nnames.get(neighborhood).localityName, redraw);
+  //   await sleep(1500);
+  //   await downloadImage(
+  //     JSON.stringify({
+  //       entry: neighborhood,
+  //       format: "png",
+  //       width,
+  //     })
+  //   );
+  //   redraw = true;
+  // }
+
+  // for await (const zipcode of zipcodes) {
+  //   update(zipcode, null, redraw);
+  //   await sleep(1500);
+  //   await downloadImage(
+  //     JSON.stringify({
+  //       entry: zipcode,
+  //       format: "png",
+  //       width,
+  //     })
+  //   );
+  //   redraw = true;
+  // }
 };
 
 window.onresize = () => {};
