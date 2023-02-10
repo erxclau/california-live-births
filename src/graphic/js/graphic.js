96b44d6511e8739c23c1f91b2375026794618baa
erxclau <ericlau41802@gmail.com>
2023-02-10 15:15:05 -0500

    Add drawer

--- src/graphic/js/graphic.js
+++ src/graphic/js/graphic.js
@@ -1,18 +1,123 @@
 // JS for your graphic
 import pym from "pym.js";
-// import * as d3 from "d3";
+import * as d3 from "d3";
+import { build, LineChart } from "@michigandaily/bore";
+import data from "../data/data.csv?autoType=false";
+import template from "../data/template.csv?autoType=false";
+import weights from "../data/weights.csv?autoType=false";
 
 import downloadImage from "./util/download-image";
 import setDisplayOptions from "./util/set-display";
 
-const draw = () => {
-  // step 1: access data
-  // step 2: create chart dimensions
-  // step 3: draw canvas
-  // step 4: create scales
-  // step 5: draw data
-  // step 6: draw peripherals
-  // step 7: set up interactions
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const draw = async () => {
+  const zipcodes = new Set(
+    template
+      .filter((d) => d.localityType === "ZIPCODE")
+      .map((d) => d.localityName)
+  );
+
+  const neighborhoods = template.filter(
+    (d) => d.localityType === "NEIGHBORHOOD"
+  );
+
+  const zdata = data
+    .map((d) => ({ year: +d.Year, zipcode: d.ZIP_Code, count: +d.Count }))
+    .filter((d) => zipcodes.has(d.zipcode));
+
+  const zmap = d3.rollup(
+    zdata,
+    (v) => new Map(v.map((d) => [new Date(d.year, 0, 0), d.count])),
+    (d) => d.zipcode
+  );
+
+  const zindex = d3.group(zdata, (d) => d.zipcode);
+  const nindex = d3.group(weights, (d) => d.leftId);
+
+  const ndata = neighborhoods
+    .map((d) => {
+      const z = nindex.get(d.localityId);
+
+      const nmap = new Map();
+      z.forEach((zip) => {
+        const w = +zip.fractionOfRightInLeft.replace("%", "") / 100;
+        if (zindex.has(zip.rightName)) {
+          const weighted = zindex
+            .get(zip.rightName)
+            .map((d) => ({ ...d, count: Math.round(d.count * w) }));
+
+          weighted.forEach((d) => {
+            if (nmap.has(d.year)) {
+              nmap.set(d.year, nmap.get(d.year) + d.count);
+            } else {
+              nmap.set(d.year, d.count);
+            }
+          });
+        }
+      });
+
+      return Array.from(nmap).map((m) => ({
+        year: m[0],
+        count: m[1],
+        id: d.localityId,
+      }));
+    })
+    .flat();
+
+  const nmap = d3.rollup(
+    ndata,
+    (v) => new Map(v.map((d) => [new Date(d.year, 0, 0), d.count])),
+    (d) => d.id
+  );
+
+  const figure = d3.select("figure");
+  const width = figure.node().clientWidth;
+
+  const svg = figure.append("svg");
+
+  const map = new Map([...zmap, ...nmap]);
+
+  const update = (id, name = null, redraw = false) => {
+    svg
+      .datum(map.get(id))
+      .call(
+        build(new LineChart().height(400).margin({ left: 50 }).redraw(redraw))
+      );
+
+    d3.select(".figure__title").text(
+      `Number of live births in ${name ?? id} since 1982`
+    );
+  };
+
+  const nnames = d3.index(neighborhoods, (d) => d.localityId);
+
+  let redraw = false;
+  for await (const neighborhood of nindex.keys()) {
+    update(neighborhood, nnames.get(neighborhood).localityName, redraw);
+    await sleep(1500);
+    await downloadImage(
+      JSON.stringify({
+        entry: neighborhood,
+        format: "png",
+        width,
+      })
+    );
+    redraw = true;
+  }
+
+  for await (const zipcode of zipcodes) {
+    update(zipcode, null, redraw);
+    await sleep(1500);
+    await downloadImage(
+      JSON.stringify({
+        entry: zipcode,
+        format: "png",
+        width,
+      })
+    );
+    redraw = true;
+  }
 };
 
 window.onresize = () => {};
--- src/graphic/js/util/download-image.js
+++ src/graphic/js/util/download-image.js
@@ -1,38 +1,17 @@
 const downloadImage = async (message) => {
-  const { entry, format, width } = JSON.parse(message);
+  const { entry, format } = JSON.parse(message);
   const a = document.createElement("a");
-  const { elementToSVG } = await import("dom-to-svg");
-  const xml = elementToSVG(document.body);
-  const svgString = new XMLSerializer().serializeToString(xml);
+  const { domToPng } = await import("modern-screenshot");
 
-  a.download = `cookie-graphic-${entry}-${width}-${new Date().toISOString()}.${format}`;
-  if (format === "svg") {
-    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(svgString)}`;
-    a.click();
-    a.remove();
-  } else if (format === "png") {
-    const img = new Image();
+  const data = await domToPng(document.body, {
+    backgroundColor: "white",
+    scale: 2,
+  });
 
-    img.onload = () => {
-      const dpr = window.devicePixelRatio ?? 1;
-
-      const canvas = document.createElement("canvas");
-      canvas.width = width * dpr;
-      canvas.height = document.body.clientHeight * dpr;
-
-      const context = canvas.getContext("2d");
-      context.scale(dpr, dpr);
-      context.fillStyle = "white";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      context.drawImage(img, 0, 0);
-
-      a.href = canvas.toDataURL("image/png");
-      a.click();
-      a.remove();
-    };
-
-    img.src = `data:image/svg+xml;base64,${window.btoa(svgString)}`;
-  }
+  a.download = `${entry}.${format}`;
+  a.href = data;
+  a.click();
+  a.remove();
 };
 
 export default downloadImage;
